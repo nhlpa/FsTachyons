1f549122eac0f1455ab5caca74df38f98ae274e4
Pim Brouwers <pbrouwers@nhlpa.com>
2022-09-14 20:51:48 -0400

    Tac file

--- cmd/GenerateClassFunctions.fsx
+++ cmd/GenerateClassFunctions.fsx
@@ -3,40 +3,15 @@ open System.IO
 open System.Net.Http
 open System.Text.RegularExpressions
 
+//
+// Tachyons
 let buildCdnUrl = "https://raw.githubusercontent.com/tachyons-css/tachyons/master/css/tachyons.css"
 let minifiedCdnUrl = "https://raw.githubusercontent.com/tachyons-css/tachyons/master/css/tachyons.min.css"
-
-let tachyonsUrlModule =
-        sprintf
-            "module TachyonsUrl =
-    /// Tachyons build CDN reference
-    let build = \"%s\"
-
-    /// Tachyons minified CDN reference
-    let minified = \"%s\""
-            buildCdnUrl
-            minifiedCdnUrl
-
 let http = new HttpClient()
-
 let rawCss = http.GetStringAsync(buildCdnUrl).Result
 
-let classNames =
-    rawCss.Split("\n")
-    |> Seq.map (fun (x : string) -> x.TrimStart()) // trim required to catch nested selectors
-    |> Seq.filter (fun x -> x.StartsWith(".")) // first reduce to "likely" selectors
-    |> Seq.filter (fun x -> Regex.IsMatch(x, @"^\.-?[_a-zA-Z]+[_a-zA-Z0-9-]* {")) // filter more specifically for valid css class names
-    |> Seq.map (fun x ->
-        let startIndex = 1
-        let ruleStartIndex = x.IndexOf("{") - 1
-        let endIndex =
-            let classSepIndex = x.IndexOf(",") |> fun x -> if x > ruleStartIndex then -1 else x
-            let pseudoIndex = x.IndexOf(":") |> fun x -> if x > ruleStartIndex then -1 else x
-            if classSepIndex > 0 then classSepIndex
-            elif pseudoIndex > 0 then pseudoIndex
-            else ruleStartIndex
-        x, x.Substring(startIndex, endIndex).Trim())
-
+//
+// Extract classes
 let functionSpecs =
     let reservedWords =
         [ "abstract"; "and"; "as"; "assert"; "base"; "begin"; "class"; "default"; "delegate"; "do"
@@ -51,7 +26,20 @@ let functionSpecs =
           "include"; "mixin"; "parallel"; "process"; "protected"; "pure"; "sealed"; "tailcall"
           "trait"; "virtual" ]
 
-    classNames
+    rawCss.Split("\n")
+    |> Seq.map (fun (x : string) -> x.TrimStart()) // trim required to catch nested selectors
+    |> Seq.filter (fun x -> x.StartsWith(".")) // first reduce to "likely" selectors
+    |> Seq.filter (fun x -> Regex.IsMatch(x, @"^\.-?[_a-zA-Z]+[_a-zA-Z0-9-]* {")) // filter more specifically for valid css class names
+    |> Seq.map (fun x ->
+        let startIndex = 1
+        let ruleStartIndex = x.IndexOf("{") - 1
+        let endIndex =
+            let classSepIndex = x.IndexOf(",") |> fun x -> if x > ruleStartIndex then -1 else x
+            let pseudoIndex = x.IndexOf(":") |> fun x -> if x > ruleStartIndex then -1 else x
+            if classSepIndex > 0 then classSepIndex
+            elif pseudoIndex > 0 then pseudoIndex
+            else ruleStartIndex
+        x, x.Substring(startIndex, endIndex).Trim())
     |> Seq.distinctBy (fun (_, className) -> className)
     |> Seq.map (fun (rule, className) ->
         let fnName = className.Replace("-", "_")
@@ -59,24 +47,27 @@ let functionSpecs =
         let fnName' = if List.contains fnName reservedWords then fnName + "'" else fnName
         sprintf "    /// %s\n    let %s = TachyonsClass \"%s\"\n" rule fnName' className)
 
-let cssClassFunctions =
+//
+// Output module templates
+let tachyonsUrlModule =
+        sprintf
+            "module TachyonsUrl =
+    /// Tachyons build CDN reference
+    let build = \"%s\"
+
+    /// Tachyons minified CDN reference
+    let minified = \"%s\""
+            buildCdnUrl
+            minifiedCdnUrl
+
+
+let tachyonsClassesModule =
     sprintf "module Tac =\n%s" (String.Join("\n", functionSpecs))
 
-let fsTachyonsSrcFile = FileInfo(Path.Join(__SOURCE_DIRECTORY__, "../src/FsTachyons/Core.fs"))
-let fsTachyonsSrcLines = File.ReadAllLines(fsTachyonsSrcFile.FullName)
+//
+// Write class function file
+let fsTachyonsSrcFile = FileInfo(Path.Join(__SOURCE_DIRECTORY__, "../src/FsTachyons/Tac.fs"))
 
-let mutable continueLooping = true
-let mutable index = 0
-
-while continueLooping do
-    let line = fsTachyonsSrcLines.[index]
-    if String.Equals(line, "// Tachyons CSS Functions", StringComparison.OrdinalIgnoreCase) then
-        continueLooping <- false
-    else
-        index <- index + 1
-
-let moduleHead = String.Join("\n", fsTachyonsSrcLines[0..index])
-
-let moduleGenerated =  String.Join("\n\n", moduleHead, tachyonsUrlModule, cssClassFunctions)
-
-File.WriteAllText(fsTachyonsSrcFile.FullName, moduleGenerated)
+File.WriteAllText(
+    fsTachyonsSrcFile.FullName,
+    String.Join("\n\n", "namespace FsTachyons", "open System", tachyonsUrlModule, tachyonsClassesModule))
